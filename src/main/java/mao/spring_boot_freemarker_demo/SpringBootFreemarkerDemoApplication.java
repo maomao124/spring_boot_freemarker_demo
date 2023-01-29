87a56c66a10434db8e0e7809dfc855760f0a5c57
mao <1296193245@qq.com>
2023-01-29 19:43:26 +0800

    spring_boot_freemarker_demo

---
+++ src/main/java/mao/spring_boot_freemarker_demo/SpringBootFreemarkerDemoApplication.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_freemarker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootFreemarkerDemoApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(SpringBootFreemarkerDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/mao/spring_boot_freemarker_demo/SpringBootFreemarkerDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package mao.spring_boot_freemarker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootFreemarkerDemoApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
